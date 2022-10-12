a0d75171fbe3228bfcc1af7dabe3bc10abb7cb97
Jon Mease <jon.mease.dev@gmail.com>
2022-10-12 19:42:11 -0400

    Add vl to image subcommands to CLI

--- vl-convert/src/main.rs
+++ vl-convert/src/main.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 
-use clap::{Parser, Subcommand, arg};
+use clap::{arg, Parser, Subcommand};
 use std::str::FromStr;
 use vl_convert_rs::converter::VlConverter;
 use vl_convert_rs::module_loader::import_map::VlVersion;
@@ -36,6 +36,42 @@ enum Commands {
         pretty: bool,
     },
 
+    /// Convert a Vega-Lite specification to an SVG image
+    #[command(arg_required_else_help = true)]
+    Vl2svg {
+        /// Path to input Vega-Lite file
+        #[arg(short, long)]
+        input: String,
+
+        /// Path to output SVG file to be created
+        #[arg(short, long)]
+        output: String,
+
+        /// Vega-Lite Version. One of 4.17, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5
+        #[arg(short, long, default_value = "5.5")]
+        vl_version: String,
+    },
+
+    /// Convert a Vega-Lite specification to an PNG image
+    #[command(arg_required_else_help = true)]
+    Vl2png {
+        /// Path to input Vega-Lite file
+        #[arg(short, long)]
+        input: String,
+
+        /// Path to output PNG file to be created
+        #[arg(short, long)]
+        output: String,
+
+        /// Vega-Lite Version. One of 4.17, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5
+        #[arg(short, long, default_value = "5.5")]
+        vl_version: String,
+
+        /// Image scale factor
+        #[arg(short, long, default_value = "1.0")]
+        scale: f32,
+    },
+
     /// Convert a Vega specification to an SVG image
     #[command(arg_required_else_help = true)]
     Vg2svg {
@@ -61,43 +97,48 @@ enum Commands {
 
         /// Image scale factor
         #[arg(short, long, default_value = "1.0")]
-        scale: f32
-    }
+        scale: f32,
+    },
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let args = Cli::parse();
     use crate::Commands::*;
     match args.command {
-        Vl2vg { input: input_vegalite_file, output: output_vega_file, vl_version, pretty } => {
-            vl_2_vg(
-                &input_vegalite_file, &output_vega_file, &vl_version, pretty
-            ).await?
-        }
-        Vg2svg { input, output } => {
-            vg_2_svg(
-                &input, &output
-            ).await?
-        }
-        Vg2png { input, output, scale } => {
-            vg_2_png(&input, &output, scale).await?
-        }
+        Vl2vg {
+            input: input_vegalite_file,
+            output: output_vega_file,
+            vl_version,
+            pretty,
+        } => vl_2_vg(&input_vegalite_file, &output_vega_file, &vl_version, pretty).await?,
+        Vl2svg {
+            input,
+            output,
+            vl_version,
+        } => vl_2_svg(&input, &output, &vl_version).await?,
+        Vl2png {
+            input,
+            output,
+            vl_version,
+            scale,
+        } => vl_2_png(&input, &output, &vl_version, scale).await?,
+        Vg2svg { input, output } => vg_2_svg(&input, &output).await?,
+        Vg2png {
+            input,
+            output,
+            scale,
+        } => vg_2_png(&input, &output, scale).await?,
     }
 
     Ok(())
 }
 
-
 fn parse_vl_version(vl_version: &str) -> Result<VlVersion, anyhow::Error> {
     if let Ok(vl_version) = VlVersion::from_str(vl_version) {
         Ok(vl_version)
     } else {
-        bail!(
-            "Invalid or unsupported Vega-Lite version: {}",
-            vl_version
-        );
+        bail!("Invalid or unsupported Vega-Lite version: {}", vl_version);
     }
 }
 
@@ -105,10 +146,7 @@ fn read_input_string(input: &str) -> Result<String, anyhow::Error> {
     match std::fs::read_to_string(&input) {
         Ok(input_str) => Ok(input_str),
         Err(err) => {
-            bail!(
-                "Failed to read input file: {}\n{}",
-                input, err
-            );
+            bail!("Failed to read input file: {}\n{}", input, err);
         }
     }
 }
@@ -126,10 +164,7 @@ fn write_output_string(output: &str, output_str: &str) -> Result<(), anyhow::Err
     match std::fs::write(&output, output_str) {
         Ok(_) => Ok(()),
         Err(err) => {
-            bail!(
-                "Failed to write converted output to {}\n{}",
-                output, err
-            );
+            bail!("Failed to write converted output to {}\n{}", output, err);
         }
     }
 }
@@ -138,10 +173,7 @@ fn write_output_binary(output: &str, output_data: &[u8]) -> Result<(), anyhow::E
     match std::fs::write(&output, output_data) {
         Ok(_) => Ok(()),
         Err(err) => {
-            bail!(
-                "Failed to write converted output to {}\n{}",
-                output, err
-            );
+            bail!("Failed to write converted output to {}\n{}", output, err);
         }
     }
 }
@@ -150,8 +182,8 @@ async fn vl_2_vg(
     input: &str,
     output: &str,
     vl_version: &str,
-    pretty: bool
-) -> Result<(), anyhow::Error>{
+    pretty: bool,
+) -> Result<(), anyhow::Error> {
     // Parse version
     let vl_version = parse_vl_version(vl_version)?;
 
@@ -181,10 +213,7 @@ async fn vl_2_vg(
     Ok(())
 }
 
-async fn vg_2_svg(
-    input: &str,
-    output: &str,
-) -> Result<(), anyhow::Error> {
+async fn vg_2_svg(input: &str, output: &str) -> Result<(), anyhow::Error> {
     // Read input file
     let vega_str = read_input_string(&input)?;
 
@@ -195,10 +224,7 @@ async fn vg_2_svg(
     let mut converter = VlConverter::new();
 
     // Perform conversion
-    let svg = match converter
-        .vega_to_svg(vg_spec)
-        .await
-    {
+    let svg = match converter.vega_to_svg(vg_spec).await {
         Ok(svg) => svg,
         Err(err) => {
             bail!("Vega-Lite to Vega conversion failed: {}", err);
@@ -211,11 +237,7 @@ async fn vg_2_svg(
     Ok(())
 }
 
-async fn vg_2_png(
-    input: &str,
-    output: &str,
-    scale: f32,
-) -> Result<(), anyhow::Error> {
+async fn vg_2_png(input: &str, output: &str, scale: f32) -> Result<(), anyhow::Error> {
     // Read input file
     let vega_str = read_input_string(&input)?;
 
@@ -226,8 +248,67 @@ async fn vg_2_png(
     let mut converter = VlConverter::new();
 
     // Perform conversion
+    let png_data = match converter.vega_to_png(vg_spec, Some(scale)).await {
+        Ok(png_data) => png_data,
+        Err(err) => {
+            bail!("Vega-Lite to Vega conversion failed: {}", err);
+        }
+    };
+
+    // Write result
+    write_output_binary(&output, &png_data)?;
+
+    Ok(())
+}
+
+async fn vl_2_svg(input: &str, output: &str, vl_version: &str) -> Result<(), anyhow::Error> {
+    // Parse version
+    let vl_version = parse_vl_version(vl_version)?;
+
+    // Read input file
+    let vegalite_str = read_input_string(&input)?;
+
+    // Parse input as json
+    let vl_spec = parse_as_json(&vegalite_str)?;
+
+    // Initialize converter
+    let mut converter = VlConverter::new();
+
+    // Perform conversion
+    let svg = match converter.vegalite_to_svg(vl_spec, vl_version).await {
+        Ok(svg) => svg,
+        Err(err) => {
+            bail!("Vega-Lite to Vega conversion failed: {}", err);
+        }
+    };
+
+    // Write result
+    write_output_string(&output, &svg)?;
+
+    Ok(())
+}
+
+async fn vl_2_png(
+    input: &str,
+    output: &str,
+    vl_version: &str,
+    scale: f32,
+) -> Result<(), anyhow::Error> {
+    // Parse version
+    let vl_version = parse_vl_version(vl_version)?;
+
+    // Read input file
+    let vegalite_str = read_input_string(&input)?;
+
+    // Parse input as json
+    let vl_spec = parse_as_json(&vegalite_str)?;
+
+    // Initialize converter
+    let mut converter = VlConverter::new();
+
+    // Perform conversion
     let png_data = match converter
-        .vega_to_png(vg_spec, Some(scale))
+        .vegalite_to_png(vl_spec, vl_version, Some(scale))
         .await
     {
         Ok(png_data) => png_data,
