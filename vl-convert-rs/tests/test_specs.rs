c55bce343e3c64b73bd1bbcab5693274a6d40a24
Jon Mease <jonmmease@gmail.com>
2022-10-21 08:16:59 -0500

    Fix width measurement of empty text (#25)
    * Add failing test with empty text
    * Add special case for measuring width of empty text

--- vl-convert-rs/tests/test_specs.rs
+++ vl-convert-rs/tests/test_specs.rs
@@ -130,7 +130,12 @@ mod test_svg {
 
     #[rstest]
     fn test(
-        #[values("circle_binned", "stacked_bar_h", "bar_chart_trellis_compact")]
+        #[values(
+            "circle_binned",
+            "stacked_bar_h",
+            "bar_chart_trellis_compact",
+            "line_with_log_scale"
+        )]
         name: &str,
     ) {
         initialize();
@@ -182,6 +187,7 @@ mod test_png {
         case("circle_binned", 1.0),
         case("stacked_bar_h", 2.0),
         case("bar_chart_trellis_compact", 2.0),
+        case("line_with_log_scale", 2.0)
     )]
     fn test(
         name: &str,
