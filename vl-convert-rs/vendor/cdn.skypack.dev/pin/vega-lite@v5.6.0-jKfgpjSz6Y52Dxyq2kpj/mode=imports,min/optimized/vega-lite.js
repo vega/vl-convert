766e60ebc12725aa7d33bcfceedbb000360b5e41
Jon Mease <jonmmease@gmail.com>
2022-10-29 07:34:51 -0400

    Add missing vendored files

---
+++ vl-convert-rs/vendor/cdn.skypack.dev/pin/vega-lite@v5.6.0-jKfgpjSz6Y52Dxyq2kpj/mode=imports,min/optimized/vega-lite.js
@@ -0,0 +1,7 @@
+/*
+ * Skypack CDN - vega-lite@5.6.0
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/-/vega-lite@v5.6.0-jKfgpjSz6Y52Dxyq2kpj/dist=es2020,mode=imports,min/optimized/vega-lite.js';
+export {default} from '/-/vega-lite@v5.6.0-jKfgpjSz6Y52Dxyq2kpj/dist=es2020,mode=imports,min/optimized/vega-lite.js';
