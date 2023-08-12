29785dc73f8f4f66ed7822381c76ae6d3e231102
Jon Mease <jonmmease@gmail.com>
2023-08-12 09:20:22 -0400

    Update tests

--- vl-convert-vendor/src/main.rs
+++ vl-convert-vendor/src/main.rs
@@ -45,8 +45,8 @@ const VL_PATHS: &[(&str, &str)] = &[
     // 5.14 is used by Altair 5.1 (keep forever)
     (
         "5.14",
-        "/pin/vega-lite@v5.14.1-0IRM1VigcIVzRzBRoLFR/mode=imports,min/optimized/vega-lite.js"
-    )
+        "/pin/vega-lite@v5.14.1-0IRM1VigcIVzRzBRoLFR/mode=imports,min/optimized/vega-lite.js",
+    ),
 ];
 const SKYPACK_URL: &str = "https://cdn.skypack.dev";
 const VEGA_PATH: &str = "/pin/vega@v5.25.0-r16knbfAAfBFDoUvoc7K/mode=imports,min/optimized/vega.js";
