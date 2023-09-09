7df79132e26b9f31110b96ec05acfecd45b75a02
Jon Mease <jonmmease@gmail.com>
2023-09-09 11:29:25 -0400

    fmt with rust 1.72

--- vl-convert-pdf/src/lib.rs
+++ vl-convert-pdf/src/lib.rs
@@ -409,7 +409,9 @@ fn node_has_zero_opacity(node: &Node) -> bool {
 }
 
 fn get_text_width(text: &Text, font_db: &Database) -> Option<f64> {
-    let Some(node) = text.convert(font_db, Default::default()) else { return None };
+    let Some(node) = text.convert(font_db, Default::default()) else {
+        return None;
+    };
     get_text_text_bbox_from_path(node)
 }
 
