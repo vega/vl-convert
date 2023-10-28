8a23c6010511f2c6a9eac76df40cbd4009ba0a38
Jon Mease <jonmmease@gmail.com>
2023-10-28 11:31:32 -0400

    Update to Vega-Lite 5.16.1 (#127)

--- vl-convert-rs/vendor/cdn.skypack.dev/pin/vega-lite@v5.16.0-pocyasPYI8XbbobxDMkp/mode=imports,min/optimized/vega-lite.js
+++
@@ -1,7 +0,0 @@
-/*
- * Skypack CDN - vega-lite@5.16.0
- * Remove "min" from url for additional help & instructions.
- */
-
-export * from '/-/vega-lite@v5.16.0-pocyasPYI8XbbobxDMkp/dist=es2020,mode=imports,min/optimized/vega-lite.js';
-export {default} from '/-/vega-lite@v5.16.0-pocyasPYI8XbbobxDMkp/dist=es2020,mode=imports,min/optimized/vega-lite.js';
