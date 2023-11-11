e6de612452c195099e96e8dfb5c5ffc7f6ac2ac0
Jon Mease <jonmmease@gmail.com>
2023-11-11 09:30:05 -0500

    Bump Vega-Lite to 5.16.3 and Vega to 5.26.1

---
+++ vl-convert-rs/vendor/cdn.skypack.dev/pin/vega-lite@v5.16.3-Hw7pZxUuaiVgThsNMjY9/mode=imports,min/optimized/vega-lite.js
@@ -0,0 +1,7 @@
+/*
+ * Skypack CDN - vega-lite@5.16.3
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/-/vega-lite@v5.16.3-Hw7pZxUuaiVgThsNMjY9/dist=es2020,mode=imports,min/optimized/vega-lite.js';
+export {default} from '/-/vega-lite@v5.16.3-Hw7pZxUuaiVgThsNMjY9/dist=es2020,mode=imports,min/optimized/vega-lite.js';
