4d04f0759ba8c5d7d427e1d58bef88e621624aaa
Jon Mease <jonmmease@gmail.com>
2024-01-18 05:57:13 -0500

    Update vega to 5.27.0 and vega-themes to 2.14.0

---
+++ vl-convert-rs/vendor/cdn.skypack.dev/pin/vega-embed@v6.24.0-w7baSqpeCYEV2w1fHJn2/mode=imports,min/optimized/vega-embed.js
@@ -0,0 +1,7 @@
+/*
+ * Skypack CDN - vega-embed@6.24.0
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/-/vega-embed@v6.24.0-w7baSqpeCYEV2w1fHJn2/dist=es2020,mode=imports,min/optimized/vega-embed.js';
+export {default} from '/-/vega-embed@v6.24.0-w7baSqpeCYEV2w1fHJn2/dist=es2020,mode=imports,min/optimized/vega-embed.js';
