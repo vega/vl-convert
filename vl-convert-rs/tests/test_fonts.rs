6ccae9ca662ccb106a9f73e72386718c0b42a2c3
Jon Mease <jonmmease@gmail.com>
2024-06-19 15:26:27 -0400

    Remove scratch test file

--- vl-convert-rs/tests/test_fonts.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn test_fonts() {
-    let mut fontdb = fontdb::Database::default();
-    // fontdb.load_system_fonts();
-    fontdb.load_fonts_dir("/Users/jonmmease/VegaFusion/repos/vl-convert/scratch/bugs/matter");
-    for face in fontdb.faces() {
-        println!("{:#?}", face);
-    }
-}
